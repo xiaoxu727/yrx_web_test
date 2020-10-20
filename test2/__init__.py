f551fa9949779b06abec5154f1083de27a3e22b7
xiaoxu727 <xuxingyuan727@163.com>
2020-10-20 17:52:34 +0800

    第2道题

---
+++ test2/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+'''
+@author: xuxingyuan
+@license: (C) Copyright 2013-2018, Node Supply Chain Manager Corporation Limited.
+@software: yrx_web_test
+@file: __init__.py.py
+@time: 2020/10/20 17:02
+@desc:
+'''
